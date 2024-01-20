66f2559a9a1e0b580430ff9f8f893c269cff5df3
Md. Shrabon Hossain <90843040+shrabon-dev@users.noreply.github.com>
2024-01-20 14:07:52 +0600

    navbar design changes

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -3,6 +3,12 @@ import logo from '../../assets/logo.png'
 import { TbMenuDeep } from "react-icons/tb";
 import { IoCloseOutline } from "react-icons/io5";
 import { GoDownload } from "react-icons/go";
+import { FaYoutube } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import hover from '../../assets/hover.png'
+import { motion,cubicBezier} from "framer-motion"
 
 export default function Navbar() {
 
@@ -34,11 +40,35 @@ export default function Navbar() {
 
             })
     },[])
+    const ulMotion = {
 
+        animate: {
+            opacity: 1,
+            transition: {
+                duration: 1,
+                staggerChildren:.35,
+            },
+        },
+    };
+
+    const liMotion = {
+        initial:{
+            opacity:0,
+            y:'20px',
+        },
+        animate:{
+            opacity:1,
+            y:'0px',
+            transition:{
+                duration:.4,
+                ease:cubicBezier(.8,.8,.8,.8),
+            }
+        }
+    }
   return (
     <>
         {/* <nav className=''> */}
-        <nav className={` bg-transparent py-4 top-14  w-full z-[999999] ${fixedMenu ? 'border-white/20 shadow-xl shadow-black/25 bg-dark/25 backdrop-blur-sm fixed border-b-[1px] rounded-b-[50px] duration-700 ease-linear' : 'absolute border-b border-transparent duration-700 ease-linear'}`}>
+        <nav className={`py-4 w-full z-[999999] ${fixedMenu ? 'shadow-xl shadow-black/25 bg-[#242424] fixed duration-700 ease-linear top-0' : 'absolute border-b bg-transparent border-transparent duration-700 ease-linear top-14'}`}>
             <div className="container mx-auto px-4 md:px-0">
                <div className="flex justify-between items-center">
                     {/* LOGO START */}
@@ -48,24 +78,48 @@ export default function Navbar() {
                            
                         </picture>
                     </div>
-                    {/* MENUS START */}
-                    <div className={`menuBars ease-linear md:block md:visible ${isOpen ? 'visible mMenu md:static ':'hidden'} bg-dark md:bg-transparent top-24 sm:top-40 right-16 sm:right-1/4 md:right-full border-t-0 w-4/6 sm:w-3/6 md:w-5/6 lg:w-5/6 xl:w-5/6 shadow-2xl md:shadow-none shadow-black rounded-md p-10 md:p-0 absolute `}>
-                        <ul className=' space-y-10 md:space-y-0  flex flex-col md:flex-row justify-between items-center'>
-                            <li><a className={`${activeMenuItem == 'home' ? 'a !text-lightGreen':'a'}`} href="#home">Home </a></li>
-                            <li><a className={`${activeMenuItem == 'about' ? 'a !text-lightGreen':'a'}`} href="#about">About</a></li>
-                            <li><a className={`${activeMenuItem == 'services' ? 'a !text-lightGreen':'a'}`} href="#services">Services</a></li>
-                            <li><a className={`${activeMenuItem == 'portfolio' ? 'a !text-lightGreen':'a'}`} href="#portfolio">Portfolio</a></li>
-                            <li><a className={`${activeMenuItem == 'feedback' ? 'a !text-lightGreen':'a'}`} href="#feedback">Feedback</a></li>
-                            <li><a className={`${activeMenuItem == 'faqs' ? 'a !text-lightGreen':'a'}`} href="#faqs">Faq's</a></li>
-                            <li><a className='a flex justify-center items-center' href="#">Info <GoDownload className='inline-block'/> </a></li>
-                        </ul>
-                        <span onClick={()=>setIsOpen(!isOpen)} className={`bg-dark w-10 h-10 flex justify-center items-center rounded-full shadow-inner animate-bounce duration-300 hover:text-lightGreen cursor-pointer shadow-black text-white md:hidden text-4xl absolute -top-4 -right-14 ${isOpen ? '':''}`}><IoCloseOutline className="inline-block"/></span>
-                    </div>
                     {/* MENU BUTTON */}
                     <div className="menuButton w-20 text-right">
                         <TbMenuDeep onClick={()=>setIsOpen(!isOpen)} className='text-white inline-block text-2xl md:text-4xl cursor-pointer'/>
                     </div>
                </div>
+                    {/* MENUS START */}
+                    <div className={`menuBars !fixed  z-0 ease-linear md:block md:visible duration-300 ${isOpen ? 'left-0 ':'-left-1/4 '} bg-[#242424] top-0 w-full md:w-1/5 h-screen  shadow-2xl md:shadow-none shadow-black  py-10`}>
+                           {/* LOGO START */}
+                    <div className="logo pt-10 text-right  pl-20">
+                        <picture>
+                            <img className='w-10 md:w-20  block' src={logo} alt={logo} />
+                           
+                        </picture>
+                    </div>
+                        <ul className='pt-20'>
+                            <li><a className={`a ${activeMenuItem == 'home' ? 'a !text-lightGreen':'a'}`} href="#home">Home </a></li>
+                            <li><a className={`a ${activeMenuItem == 'about' ? 'a !text-lightGreen':'a'}`} href="#about">About</a></li>
+                            <li><a className={`a ${activeMenuItem == 'services' ? 'a !text-lightGreen':'a'}`} href="#services">Services</a></li>
+                            <li><a className={`a ${activeMenuItem == 'portfolio' ? 'a !text-lightGreen':'a'}`} href="#portfolio">Portfolio</a></li>
+                            <li><a className={`a ${activeMenuItem == 'feedback' ? 'a !text-lightGreen':'a'}`} href="#feedback">Feedback</a></li>
+                            <li><a className={`a ${activeMenuItem == 'faqs' ? 'a !text-lightGreen':'a'}`} href="#faqs">Faq's</a></li>
+                            <li><a className='a flex justify-start items-center gap-2 !border-b-0' href="#">Info <GoDownload className='inline-block'/> </a></li>
+                        </ul>
+                        {/* Contact Me Start*/}
+
+                        <div className="contact_me p-20">
+                            <h6 className='p-0 text-base font-medium uppercase text-white/75 border-b inline pb-1 border-lightGreen font-mulish'>Contact Me</h6>
+                            <p className='p-0 text-sm font-medium  text-white/75 pt-4  font-mulish'>Zinzira, Keranigonj-Dhaka 1210</p>
+                            <p className='p-0 text-sm font-medium  text-white/75 pt-1  font-mulish'>Whatsapp: +880 1786119237</p>
+                            
+                            <motion.ul variants={ulMotion}  viewport={{ once:true }} className='flex gap-4 pt-10 -ml-4'>
+                        <motion.li variants={liMotion}><a className='w-10 h-10 mr-1 px-4 inline-block text-white text-2xl relative group' href="#"><FaYoutube className='relative z-10'/> <span className='absolute z-0 left-1/2 top-1/2 w-full -translate-x-[27%] -translate-y-[62%] opacity-0 group-hover:opacity-100 duration-300 ease-linear'><img className='block w-[200px]' src={hover} alt={hover} /></span></a></motion.li>
+                        <motion.li variants={liMotion}><a className='w-10 h-10 mr-1 px-4 inline-block text-white text-2xl relative group' href="#"><FaInstagram className='relative z-10'/> <span className='absolute z-0 left-1/2 top-1/2 w-full -translate-x-[27%] -translate-y-[62%] opacity-0 group-hover:opacity-100 duration-300 ease-linear'><img className='block w-[200px]' src={hover} alt={hover} /></span></a></motion.li>
+                        <motion.li variants={liMotion}><a className='w-10 h-10 mr-1 px-4 inline-block text-white text-2xl relative group' href="#"><BsTwitterX className='relative z-10'/> <span className='absolute z-0 left-1/2 top-1/2 w-full -translate-x-[27%] -translate-y-[62%] opacity-0 group-hover:opacity-100 duration-300 ease-linear'><img className='block w-[200px]' src={hover} alt={hover} /></span></a></motion.li>
+                        <motion.li variants={liMotion}><a className='w-10 h-10 mr-1 px-4 inline-block text-white text-2xl relative group' href="#"><FaLinkedin className='relative z-10'/> <span className='absolute z-0 left-1/2 top-1/2 w-full -translate-x-[27%] -translate-y-[62%] opacity-0 group-hover:opacity-100 duration-300 ease-linear'><img className='block w-[200px]' src={hover} alt={hover} /></span></a></motion.li>
+                        </motion.ul>
+                        </div>
+
+
+
+                        <span onClick={()=>setIsOpen(!isOpen)} className={`bg-dark w-10 h-10 flex justify-center items-center rounded-full shadow-inner animate-bounce duration-300 hover:text-lightGreen cursor-pointer shadow-black text-white md:hidden text-4xl absolute -top-4 -right-14 ${isOpen ? '':''}`}><IoCloseOutline className="inline-block"/></span>
+                    </div>
             </div>
         </nav>
     </>
